b094a7e78bed42591d7ed01d4c3210f7521f1eb3
DhanashreeA <adhikari.dhanashree@gmail.com>
2017-01-14 00:46:22 -0700

    classes : ice and accounts

---
+++ models/user_account.py
@@ -0,0 +1,8 @@
+class user_accounts():
+    def __init__(self, user_id, username, password, timestamp):
+        self.user_id = user_id
+        self.username = username
+        self.password = password
+        self.timestamp = timestamp
+
+
