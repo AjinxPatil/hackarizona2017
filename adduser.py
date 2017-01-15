e7e476ed3f32072c8095d452abf75bca17100265
DhanashreeA <adhikari.dhanashree@gmail.com>
2017-01-14 17:43:11 -0700

    Decorator added for flask

--- adduser.py
+++ adduser.py
@@ -1,12 +1,16 @@
 import user_account
 from pymongo import MongoClient
+from flask import request 
 
 client = MongoClient('localhost', 27017)
 db = client['hackaz17']
 collection = db['user_accounts']
 
-def addusertodb(payload):
-    newuser = user_account.user_accounts(payload['user_id'], payload['username'], payload['password'], payload['timestamp'])
+adduser_api = Blueprint('adduser_api', __name__) 
+
+@adduser_api.route('/adduser', methods=['POST'])
+def addusertodb():
+    newuser = user_account.user_accounts(request.form['user_id'], request.form['username'], request.form['password'], request.form['timestamp'])
     collection.insert(newuser.__dict__)
 
 
